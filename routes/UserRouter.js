7c52345ae9a26f209022a8468a405b29df7d4204
Brian Lara <Briandlara1@gmail.com>
2022-11-11 12:48:23 -0500

    created variables on app router

---
+++ routes/UserRouter.js
@@ -0,0 +1,5 @@
+const Router = require('express').Router()
+const controller = require('../controllers/UserController')
+const middleware = require('../middleware')
+
+Router.get('/', controller.GetUsers)
