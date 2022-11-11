764699331646d705a4146143151de266f9cf37e8
Brian Lara <Briandlara1@gmail.com>
2022-11-11 12:41:27 -0500

    added routes to approuter.js

--- controllers/AppRouter.js
+++ controllers/AppRouter.js
@@ -2,3 +2,7 @@ const Router = require('express').Router()
 // const UserRouter = require('./')
 // const ZodiacRouter = require('./StudentRouter')
 // const ReviewsRouter = require('./StudentRouter')
+
+Router.use('/user', StudentRouter)
+Router.use('/zodiac', TeacherRouter)
+Router.use('/review', HouseRouter)
