85dafb7b0c5726603befac52a8deaffc6912c68c
Brian Lara <Briandlara1@gmail.com>
2022-11-11 12:51:40 -0500

    created getUsers controller function

--- controllers/AppRouter.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express').Router()
-const UserRouter = require('../routes/UserRouter')
-const ZodiacRouter = require('../routes/ZodiacRouter')
-const ReviewRouter = require('../routes/ReviewRouter')
-
-Router.use('/user', UserRouter)
-Router.use('/zodiac', ZodiacRouter)
-Router.use('/review', ReviewRouter)
