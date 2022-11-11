002b7af6e1c1569623ae3b7c35134a26214bb312
Brian Lara <Briandlara1@gmail.com>
2022-11-11 12:53:40 -0500

    created get user byid function

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,23 @@
 const { User } = require('../models')
+const middleware = require('../middleware')
+const { Op } = require('sequelize')
 
-const getAllUsers = () => {}
+const GetUsers = async (req, res) => {
+  try {
+    const users = await User.findAll()
+    res.send(users)
+  } catch (error) {
+    throw error
+  }
+}
+
+const GetUserById = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id)
+    res.send(user)
+  } catch (error) {
+    throw error
+  }
+}
+
+const createUser = async (req, res) => {}
