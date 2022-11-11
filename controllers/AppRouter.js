510787f66b8acb81e38b97bbb2ef932563c5ff4c
Brian Lara <Briandlara1@gmail.com>
2022-11-11 12:40:16 -0500

    added approuter file

---
+++ controllers/AppRouter.js
@@ -0,0 +1,4 @@
+const Router = require('express').Router()
+// const UserRouter = require('./')
+// const ZodiacRouter = require('./StudentRouter')
+// const ReviewsRouter = require('./StudentRouter')
