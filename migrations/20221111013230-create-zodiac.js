70ed657a457084de4ccecdb316ccc6a5d69d813b
Stephan D Ramalho <sramalho@fordham.edu>
2022-11-12 12:28:26 -0500

    changes to zodiac model and seeder

--- migrations/20221111013230-create-zodiac.js
+++ migrations/20221111013230-create-zodiac.js
@@ -18,9 +18,9 @@ module.exports = {
       description: {
         type: Sequelize.TEXT
       },
-      userId: {
-        type: Sequelize.INTEGER
-      },
+      // userId: {
+      //   type: Sequelize.INTEGER
+      // },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
