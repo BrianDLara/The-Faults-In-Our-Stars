413717a4305bea5ad61819e5adb599553969cbb4
Stephan D Ramalho <sramalho@fordham.edu>
2022-11-14 16:40:14 -0500

    changes to user seeder

--- seeders/20221112174142-user.js
+++ seeders/20221112174142-user.js
@@ -5,6 +5,43 @@ const falso = require('@ngneat/falso')
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    const userImages = [
+      'https://i.imgur.com/6ETfboe.jpeg',
+      'https://i.imgur.com/xsUQMTv.jpeg',
+      'https://i.imgur.com/8NzOvCm.jpeg',
+      'https://i.imgur.com/FdtVVNk.jpeg',
+      'https://i.imgur.com/xsV0sDX.jpeg',
+      'https://i.imgur.com/J65dL02.jpeg',
+      'https://i.imgur.com/jYWNuYA.jpeg',
+      'https://i.imgur.com/1QyvYbdb.jpg',
+      'https://i.imgur.com/dXO886h.jpeg',
+      'https://i.imgur.com/Tl1ieNZ.jpeg',
+      'https://i.imgur.com/Fl4JkDf.jpeg',
+      'https://i.imgur.com/AsQxXpl.jpeg',
+      'https://i.imgur.com/aq6aFiv.jpeg',
+      'https://i.imgur.com/CXmZzBF.png',
+      'https://i.imgur.com/i3WmsKd.jpeg',
+      'https://i.imgur.com/nxxrJ02.jpeg',
+      'https://i.imgur.com/0n1n6YT.jpeg',
+      'https://i.imgur.com/q2t3Vds.jpeg',
+      'https://i.imgur.com/1k6iW79b.jpg',
+      'https://i.imgur.com/PbFmHrob.jpg',
+      'https://i.imgur.com/CBPG6Wt.jpeg',
+      'https://i.imgur.com/nE3Tjn9b.jpg',
+      'https://i.imgur.com/ZC27dqd.jpeg',
+      'https://i.imgur.com/2DCExxN.jpeg',
+      'https://i.imgur.com/DrPwUZx.jpeg',
+      'https://i.imgur.com/56dMwBCb.jpg',
+      'https://i.imgur.com/mNKcp3U.jpeg',
+      'https://i.imgur.com/IIoqTOT.jpeg',
+      'https://i.imgur.com/JP54dSfb.jpg',
+      'https://i.imgur.com/BMX1DY4.jpeg',
+      'https://i.imgur.com/AvLYnTN.jpeg',
+      'https://i.imgur.com/a5vaGBw.jpeg',
+      'https://i.imgur.com/3P5E9AN.jpeg',
+      'https://i.imgur.com/rgSsqWq_d.webp?maxwidth=520&shape=thumb&fidelity=high',
+      'https://i.imgur.com/mry5qHS.jpeg'
+    ]
     const users = await Promise.all(
       [...Array(100)].map(async () => {
         // const user = await User.findOne({
@@ -20,7 +57,7 @@ module.exports = {
 
         return {
           username: falso.randUserName(),
-          image: falso.randAvatar({ size: 600 }),
+          image: userImages,
           firstName: falso.randFirstName(),
           lastName: falso.randLastName(),
           description: falso.randQuote(),
